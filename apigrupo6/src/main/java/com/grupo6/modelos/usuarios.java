68c96a5ab6f354c5a04e62f7870b2e539160ab5c
Damayac59 <Damaya59@gmail.com>
2021-10-15 21:32:40 -0500

    creacion Apirest
    Creación del apigrupo6

---
+++ apigrupo6/src/main/java/com/grupo6/modelos/usuarios.java
@@ -0,0 +1,9 @@
+package com.grupo6.modelos;
+
+public class usuarios {
+    private String nombres;
+    private String apellidos;
+    private int documento;
+    private String diasLaborales;
+    
+}
