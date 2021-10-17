7a42e70f0ee2a0a7f2f86ff10419d57ed00e94ca
Damayac59 <Damaya59@gmail.com>
2021-10-17 09:23:58 -0500

    PostMappin guardar estudiante
    Post mapping del body en el controlador de usuario

--- apigrupo6/src/main/java/com/grupo6/apigrupo6/controlador/UsuarioControlador.java
+++ apigrupo6/src/main/java/com/grupo6/apigrupo6/controlador/UsuarioControlador.java
@@ -1,5 +1,11 @@
 package com.grupo6.apigrupo6.controlador;
 
+import com.grupo6.apigrupo6.servicios.UsuarioServicio;
+import com.grupo6.modelos.UsuarioModelo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("*/usuario")
 public class UsuarioControlador {
     
+    @Autowired
+    UsuarioServicio usuarioServicio;
+
+    @PostMapping
+    public UsuarioModelo guardarUsuario(@RequestBody UsuarioModelo usuario){
+        return usuarioServicio.guardarUsuario(usuario);
+    }
 }
